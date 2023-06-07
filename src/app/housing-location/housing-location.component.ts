e4f5993a21f3fc6a7140935754358ff1fc68943a
VeLuSiEs <97045041+VeLuSiEs@users.noreply.github.com>
2023-06-07 14:56:01 +0100

    2 files modified

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -7,9 +7,11 @@ import { HousingLocation } from '../housinglocation';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location funciona!
-    </p>
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="fotografia {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
   `,
   styleUrls: ['./housing-location.component.css'],
 })
