600b5d7ca7f07c51344124cdaa9003a02c7ca748
VeLuSiEs <97045041+VeLuSiEs@users.noreply.github.com>
2023-06-10 16:08:12 +0100

    1 binaries uploaded

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -40,7 +40,7 @@ export class HomeComponent {
       name: 'A113 Transitional Housing',
       city: 'Santa Monica',
       state: 'CA',
-      photo: '/assets/ref1.png',
+      photo: '/assets/referee.png',
       availableUnits: 0,
       wifi: false,
       laundry: true,
