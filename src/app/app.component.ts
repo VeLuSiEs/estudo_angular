797b21fa2a0b7b211439e8616c9817878eb8b53c
VeLuSiEs <97045041+VeLuSiEs@users.noreply.github.com>
2023-06-12 11:41:06 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { HousingLocationComponent } from './housing-location/housing-location.co
   <main>
     <header class="brand-name">
       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
+    </header> 
     <section class="content">
       <app-home></app-home>
     </section>
