9d4020a3602609f466a75d08d26ad9d91e49aa67
VeLuSiEs <97045041+VeLuSiEs@users.noreply.github.com>
2023-06-12 14:26:49 +0100

    1 binaries uploaded

---
+++ src/app/routes.ts
@@ -0,0 +1,19 @@
+// This file is where we will define the routes in the application.
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+const routeConfig: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Home details',
+  },
+];
+
+export default routeConfig;
